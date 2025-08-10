3030a6ec28f6e5e12e724b116487b6d6c5473a9e
uniadmin1 <bizinquiries@gouni.co>
2025-08-10 19:15:24 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const callClaudeAPI = async (prompt, maxTokens = 1000) => {
     throw error;
   }
 };
-    }
+    };
     
     const data = await response.json();
     return data.content[0].text;
